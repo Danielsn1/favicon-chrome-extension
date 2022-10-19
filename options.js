484bec233b67e93ffd81542d16ce5deab55380e3
Danielsn1 <50124809+Danielsn1@users.noreply.github.com>
2022-10-19 08:49:53 -0500

    Formated options page
    Added CSS to options page so that the image and url would be shown
    inline with a gap between the image and the url

--- options.js
+++ options.js
@@ -1,52 +1,34 @@
-let page = document.getElementById("buttonDiv");
-let test = document.getElementById("test");
-let selectedClassName = "current";
-const presetButtonColors = ["#3aa757", "#e8453c", "#f9bb2d", "#4688f1"];
+let wrapper = document.getElementById("urlsWrapper");
 
-function handleButtonClick(event) {
-    let current = event.target.parentElement.querySelector(
-        `.${selectedClassName}`
-    );
-    if (current && current !== event.target) {
-        current.classList.remove(selectedClassName);
-    }
+// function handleButtonClick(event) {
+//     let current = event.target.parentElement.querySelector(
+//         `.${selectedClassName}`
+//     );
+//     if (current && current !== event.target) {
+//         current.classList.remove(selectedClassName);
+//     }
 
-    let color = event.target.dataset.color;
-    event.target.classList.add(selectedClassName);
-    chrome.storage.sync.set({ color })
-}
+//     let color = event.target.dataset.color;
+//     event.target.classList.add(selectedClassName);
+//     chrome.storage.sync.set({ color })
+// }
 
-function constructOptions(buttonColors) {
-    chrome.storage.sync.get("color", (data) => {
-        chrome.storage.sync.get(null, (keyValues) => {
-            let currentColor = data.color;
-            console.log(keyValues);
+function constructOptions() {
+    chrome.storage.sync.get(null, (keyValues) => {
+        console.log(keyValues);
 
-            for (let buttonColor of buttonColors) {
-                let button = document.createElement("button");
-                button.dataset.color = buttonColor;
-                button.style.backgroundColor = buttonColor;
-
-
-                if (buttonColor === currentColor) {
-                    button.classList.add(selectedClassName);
-                }
-
-                button.addEventListener("click", handleButtonClick);
-                page.appendChild(button);
-            }
-
-            for (let [location, { url }] of Object.entries(keyValues)) {
-                console.log(url);
-                let p = document.createElement("p");
-                let image = document.createElement("img");
-                p.innerHTML = `URL: ${location} <br> Image: ${url}`
-                image.src = url;
-                test.appendChild(p);
-                test.appendChild(image);
-            }
-        });
+        for (let [location, { url }] of Object.entries(keyValues)) {
+            let line = document.createElement("div");
+            line.className = "line";
+            let p = document.createElement("p");
+            let image = document.createElement("img");
+            p.innerHTML = `${location}`;
+            image.src = url;
+            line.appendChild(image);
+            line.appendChild(p);
+            wrapper.appendChild(line);
+        }
     });
 }
 
-constructOptions(presetButtonColors);
+constructOptions();
