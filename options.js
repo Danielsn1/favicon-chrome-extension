329b4d5f08197d130c54f1b146ce26f8c2477d86
Danielsn1 <50124809+Danielsn1@users.noreply.github.com>
2022-11-08 23:56:19 -0600

    Added ability to delete entries
    Made it so that links can be deleted so that they will no longer get
    overwritten.

--- options.js
+++ options.js
@@ -1,4 +1,5 @@
 let wrapper = document.getElementById("urlsWrapper");
+let button = document.getElementById("deleteButton");
 
 // function handleButtonClick(event) {
 //     let current = event.target.parentElement.querySelector(
@@ -15,23 +16,48 @@ let wrapper = document.getElementById("urlsWrapper");
 
 function constructOptions() {
     chrome.storage.sync.get(null, (keyValues) => {
-        console.log(keyValues);
-
+        let count = 0
         for (let [location, { url }] of Object.entries(keyValues)) {
-            let line = document.createElement("div");
-            line.className = "line";
-            let p = document.createElement("p");
-            let image = document.createElement("img");
-            p.innerHTML = `${location}`;
-            image.src = url;
-            let brk = document.createElement("div");
-            brk.className = "break";
-            line.appendChild(image);
-            line.appendChild(p);
-            wrapper.appendChild(line);
-            wrapper.appendChild(brk);
+            if (location != "default") {
+                count++;
+                let check = document.createElement("input");
+                check.setAttribute("type", "checkbox")
+                check.id = `checkbox${count}`;
+
+                let label = document.createElement("label");
+                label.setAttribute("for", `checkbox${count}`)
+
+                let line = document.createElement("div");
+                line.className = "line";
+
+                let a = document.createElement("a");
+                a.innerHTML = `${location}`;
+                a.href = `${location}`;
+
+                let image = document.createElement("img");
+                image.src = url;
+
+                let brk = document.createElement("div");
+                brk.className = "break";
+
+                line.appendChild(image);
+                line.appendChild(a);
+                label.appendChild(line);
+                wrapper.append(check, label, brk);
+            }
         }
     });
 }
 
 constructOptions();
+
+button.addEventListener('click', (e) => {
+    let checks = document.querySelectorAll("input[type='checkbox']")
+    checks.forEach((check) => {
+        if (check.checked) {
+            console.log(check.labels[0].innerText);
+            chrome.storage.sync.remove(check.labels[0].innerText);
+        }
+    });
+    location.reload();
+});
