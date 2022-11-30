771226d2e413b5e1a123c1fb698dcfccfbeb9d9f
Danielsn1 <50124809+Danielsn1@users.noreply.github.com>
2022-11-30 07:34:23 -0600

    Updated popup
    Added a button to clear the selected image for the url. Changed styling
    of image to float instead of changing border style. Added checks so that
    chrome file limits are not broken by the user.

--- popup.js
+++ popup.js
@@ -1,5 +1,11 @@
+const MAX_ITEMS = 512;
+const MAX_BYTES = 102400;
+const MAX_BYTES_PER_ITEM = 8192;
+
 let fileReader = document.querySelector("input");
 let image = document.getElementById("inputImage")
+let clear = document.getElementById("clear");
+let dropZone = document.getElementById("drop");
 let reader = new FileReader();
 let url;
 
@@ -21,11 +27,32 @@ chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
             [tabs[0].url]: { url }
         });
         window.close();
-    }, false)
+    }, false);
 
-    fileReader.addEventListener("change", () => {
-        let file = fileReader.files[0];
-        //let file_blob = new Blob([file.arrayBuffer()], { "type": file.type })
-        reader.readAsDataURL(file);
+    dropZone.addEventListener("drop", (e) => {
+        e.preventDefault();
+        if (e.dataTransfer.items && e.dataTransfer.files.length === 1) {
+            reader.readAsDataURL(e.dataTransfer.files[0])
+        }
+        else {
+            console.error("Incorrect number of files uploaded. Only one file is allowed at a time.")
+        }
+    });
+
+    dropZone.addEventListener("dragover", (e) => {
+        e.preventDefault();
     })
+
+    fileReader.addEventListener("change", async () => {
+        let file = fileReader.files[0];
+        if (Object.keys(await chrome.storage.sync.get()).length + 1 > MAX_ITEMS) console.error("Number of URL's has been exceded.");
+        else if (file.size > MAX_BYTES_PER_ITEM) console.error("Image provided is too large.");
+        else if (await chrome.storage.sync.getBytesInUse() + file.size > MAX_BYTES) console.error("Number of Bytes has been exceded.");
+        else reader.readAsDataURL(file);
+    })
+
+    clear.addEventListener("click", () => {
+        chrome.storage.sync.remove(currTabUrl);
+        location.reload();
+    });
 });
